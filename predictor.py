c2fae1dd65dc4abbf60503130b128cb92cc281a8
Tahmeed Chowdhury <41610281+tahmeedchowdhury@users.noreply.github.com>
2023-03-29 19:02:25 -0400

    Add files via upload

--- predictor.py
+++ predictor.py
@@ -1,30 +1,41 @@
 from ultralytics import YOLO
 import time
 import math
+import tkinter
 import cv2 as cv
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 def processOneImage(specifiedRegions, boundingBoxes, timeSeries):
 
-    # Assumes boxes given in format [number,x1,y1,x2,y2]
-    regionCounts = np.zeros(len(specifiedRegions))
+    # Assumes boxes given in format [x1,y1,x2,y2]
+    regionCounts = []
+    for i in range(len(specifiedRegions)):
+        regionCounts.append(0)
     for box in boundingBoxes:
         for i in range(len(specifiedRegions)):
             if isBoundingBoxInRegion(box, specifiedRegions[i]):
-                regionCounts[i] += abs(box[2] - box[4])
+                regionCounts[i] += abs(box[3].item() - box[1].item()) ** 2
+                break
 
-    for i in range(len(timeSeries)):
-        np.append(timeSeries[i], regionCounts[i])
+    if not timeSeries:
+        for i in range(len(specifiedRegions)):
+            timeSeries.append([regionCounts[i]])
+    else:
+        for i in range(len(specifiedRegions)):
+            timeSeries[i].append(regionCounts[i])
 
     return timeSeries
 
 
 def isBoundingBoxInRegion(box, region):
 
-    # Assumes boxes and regions given in format [number,x1,y1,x2,y2]
-    center = [(box[1] + box[3]) / 2, (box[2] + box[4]) / 2]  # Average x and y boundaries of bounding box to get its center
-    if (center[0] <= region[1] and center[0] >= region[3] or center[0] >= region[1] and center[0] <= region[3]) and (
-            center[1] <= region[2] and center[1] >= region[4] or center[1] <= region[2] and center[1] >= region[4]):
+    # Assumes boxes given in format [x1,y1,x2,y2] and regions given in format [number,x1,y1,x2,y2]
+    center = [(box[0] + box[2]) / 2, (box[1] + box[3]) / 2]  # Average x and y boundaries of bounding box to get its center
+    if (center[0].item() <= region[0] and center[0].item() >= region[2] or center[0].item() >= region[0] and center[0].item() <= region[2]) and (
+            center[1].item() <= region[1] and center[1].item() >= region[3] or center[1].item() >= region[1] and center[1].item() <= region[3]):
         return True
     return False
 
@@ -47,38 +58,50 @@ def generateWarnings(specifiedRegions, timeSeries, occupancyValueLimit, occupanc
                         specifiedRegions[i]) + ") - Long-Term Occupancy Increase Has Exceeded Limit")
 
 
-
 def main():
 
     specifiedRegions = []  # List containing lists - each element is a list corresponding to one region and contains its coordinates
     timeSeries = []
-    for i in range(len(specifiedRegions)):
-        timeSeries.append(np.empty(shape=(0, 0)))
 
     # Arbitrarily Chosen Values
-    occupancyValueLimit = 100
-    occupancyShortTermIncreaseLimit = 10
-    occupancyLongTermIncreaseLimit = 50
+    occupancyValueLimit = 200
+    occupancyShortTermIncreaseLimit = 100
+    occupancyLongTermIncreaseLimit = 150
 
-    while(True):
+    imageLength = 500
+    imageWidth = 700
 
-        # PLACEHOLDER for Receiving New Image
-
-        # PLACEHOLDER for YOLO computation generating bounding boxes:
-        boundingBoxes = []  # List containing lists - each element is a list corresponding to one bounding box and contains its coordinates
-
-        timeSeries = processOneImage(specifiedRegions, boundingBoxes, timeSeries)
-
-        generateWarnings(specifiedRegions, timeSeries, occupancyValueLimit, occupancyShortTermIncreaseLimit, occupancyLongTermIncreaseLimit)
-
-def yolo():
-    model = YOLO("yolov8n.pt") #model
-    results = model("C:/Users/CPS/Pictures/capstone image collections/datasets/Yard 3 3-6-2023/image30.png")  # predict on an image
+    model = YOLO("yolov8n.pt")  # model
+    results = model("C:/Users/CPS/Pictures/capstone image collections/datasets/Yard 4 3-6-23")  # predict on an image
+    #time.sleep(5)
     boxes = []
     for res in results:
+        boxes = []
         for i in range(len(res.boxes.cls)):
             if res.boxes.cls[i] == 0:
                 boxes.append(res.boxes.xyxy[i])
-        #Put All Prediction code here
-yolo()
-print("its done")
+
+        numHorizontalBoxes = 7
+        numVerticalBoxes = 5
+
+        if not specifiedRegions:
+            for i in range(numVerticalBoxes):
+                for j in range(numHorizontalBoxes):
+                    specifiedRegions.append([int(j*imageWidth/numHorizontalBoxes), int(i*imageLength/numVerticalBoxes), int((j+1)*imageWidth/numHorizontalBoxes), int((i+1)*imageLength/numVerticalBoxes)])
+
+        timeSeries = processOneImage(specifiedRegions, boxes, timeSeries)
+
+        generateWarnings(specifiedRegions, timeSeries, occupancyValueLimit, occupancyShortTermIncreaseLimit, occupancyLongTermIncreaseLimit)
+
+    for i in range(len(timeSeries)):
+        plt.subplot(numVerticalBoxes, numHorizontalBoxes, i + 1)
+        plt.plot(timeSeries[i])
+        #plt.title('Region (' + str(specifiedRegions[i][0]) + ', ' + str(specifiedRegions[i][1]) +
+        #          '), (' + str(specifiedRegions[i][2]) + ', ' + str(specifiedRegions[i][3]) + ')')
+    
+    plt.show()
+
+
+main()
+time.sleep(1)
+print("It is done!")
